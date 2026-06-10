b0d8426351c70d21be07d25b88843ec53362f181
methodelogistiquesncf-boop <methodelogistiquesncf@gmail.com>
2026-06-10 21:02:08 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "VOTRE_API_KEY",
-    authDomain: "VOTRE_PROJET.firebaseapp.com",
-    projectId: "VOTRE_PROJET",
-    storageBucket: "VOTRE_PROJET.appspot.com",
-    messagingSenderId: "VOTRE_MESSAGING_ID",
-    appId: "VOTRE_APP_ID"
+  apiKey: "AIzaSyD_fJOPa6_dkRn69yexIsk3qsInDWs5GIk",
+  authDomain: "coupedumonde2026-5046b.firebaseapp.com",
+  projectId: "coupedumonde2026-5046b",
+  storageBucket: "coupedumonde2026-5046b.firebasestorage.app",
+  messagingSenderId: "1010691765876",
+  appId: "1:1010691765876:web:8280c531ee6b0ecf098321"
 };
 
 const app = initializeApp(firebaseConfig);
